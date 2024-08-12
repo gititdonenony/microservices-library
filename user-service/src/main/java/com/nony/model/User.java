5ab1731b9e4d0d3d6532a0c250b126e72f57ee72
nony <navid.nony@gmail.com>
2024-08-12 08:42:03 +0530

    initial commit

---
+++ user-service/src/main/java/com/nony/model/User.java
@@ -0,0 +1,18 @@
+package com.nony.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+}
